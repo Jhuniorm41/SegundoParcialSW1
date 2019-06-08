89ba9f6d4121fe090cc4cf67e4c9926e1d7ef2cf
jhuniorm41 <jguzman@mc4.com.bo>
2019-06-08 01:31:00 -0400

    biometricoADD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage';
+import { AndroidFingerprintAuth } from '@ionic-native/android-fingerprint-auth/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot({
+    name: '__demo-touch-id-db',
+    driverOrder: ['indexeddb', 'sqlite', 'websql']
+  })],
   providers: [
     StatusBar,
     SplashScreen,
+    AndroidFingerprintAuth,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AndroidFingerprintAuth } from '@ionic-native/android-fingerprint-auth/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private androidFingerprintAuth: AndroidFingerprintAuth) { }
 
+  auntenticar() {
+    this.androidFingerprintAuth.isAvailable()
+    .then((result) => {
+      if (result.isAvailable) {
+        // it is available
+        this.androidFingerprintAuth.encrypt({ clientId: 'myAppName', username: 'myUsername', password: 'myPassword' })
+          // tslint:disable-next-line:no-shadowed-variable
+          .then(result => {
+            if (result.withFingerprint) {
+              console.log('Successfully encrypted credentials.');
+              console.log('Encrypted credentials: ' + result.token);
+            } else if (result.withBackup) {
+              console.log('Successfully authenticated with backup password!');
+            } else { console.log('Didn\'t authenticate!'); }
+          })
+          .catch(error => {
+            if (error === this.androidFingerprintAuth.ERRORS.FINGERPRINT_CANCELLED) {
+              console.log('Fingerprint authentication cancelled');
+            } else { console.error(error); }
+          });
+      } else {
+        // fingerprint auth isn't available
+      }
+    }).catch(error => console.error(error));
+  }
 }
+
